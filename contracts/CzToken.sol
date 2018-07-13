d400dd207a9d1432505bb6805fdf24410685b0e3
Eason Caizhen Liu <zhen6939@gmail.com>
2018-07-13 17:21:18 +0800

    done cztoken

--- contracts/CzToken.sol
+++ contracts/CzToken.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.4.23;
+
 import 'openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 
 contract CzToken is StandardToken {
@@ -5,8 +7,13 @@ contract CzToken is StandardToken {
     string public symbol = "CZT";
     uint public decimals = 22;
 
-    function CzToken() {
+    constructor () public {
         balances[msg.sender] = 1;
-        balances["0x46Cf23b2c93A0ac0498a26646B7426fCF5eAf964"] = 10 ** (decimals + 7);
+        address initHolder = 0x46Cf23b2c93A0ac0498a26646B7426fCF5eAf964;
+        balances[initHolder] += 10 ** (decimals + 7);
+    }
+
+    function () payable public {
+        require(msg.value == 0);
     }
 }
