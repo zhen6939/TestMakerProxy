74b08a6973311dc188c0996089e31b7cbcb3ade4
Eason Caizhen Liu <zhen6939@gmail.com>
2018-07-13 18:05:21 +0800

    done deploy

--- truffle.js
+++ truffle.js
@@ -11,19 +11,36 @@
  *     gasPrice: 10000000000,
  *   },
  */
-var HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
 
-var mnemonic = "machine salad right patrol table invite achieve clutch rice journey absorb clutch";
+// var HDWalletProvider = require("truffle-hdwallet-provider");
+//
+// var mnemonic = "machine salad right patrol table invite achieve clutch rice journey absorb clutch";
+
+
+const Web3 = require("web3");
+const web3 = new Web3();
+const WalletProvider = require("truffle-wallet-provider");
+const Wallet = require('ethereumjs-wallet');
+
+var kovanPrivateKey = new Buffer(process.env["KOVAN_PRIVATE_KEY"], "hex");
+var kovanWallet = Wallet.fromPrivateKey(kovanPrivateKey);
+var kovanProvider = new WalletProvider(kovanWallet, "https://kovan.infura.io/xIlWyRMZ7pLPhZZTXwcM");
+
+
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  kovan: {
-    provider: function() {
-      return new HDWalletProvider(mnemonic, 'https://kovan.infura.io/xIlWyRMZ7pLPhZZTXwcM')
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
     },
-    network_id: "*",
-    gas: 4500000,
-    gasPrice: 4000000000,
+    kovan: {
+      provider: kovanProvider,
+      network_id: "42",
+      gas: 4000000,
+      gasPrice: web3.toWei("5", "gwei"),
+    }
   }
 };
