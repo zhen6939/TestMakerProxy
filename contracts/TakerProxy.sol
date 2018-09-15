88815d1ee30d136a5196ecd882b473efa9ff0ec7
Eason Caizhen Liu <zhen6939@gmail.com>
2018-09-15 11:02:52 +0800

    remove import

--- contracts/TakerProxy.sol
+++ contracts/TakerProxy.sol
@@ -1,6 +1,28 @@
 pragma solidity ^0.4.23;
 
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+// copy from openzeppelin-solidity
+
+contract ERC20Basic {
+    function totalSupply() public view returns (uint256);
+    function balanceOf(address who) public view returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+contract ERC20 is ERC20Basic {
+    function allowance(address owner, address spender)
+        public view returns (uint256);
+
+    function transferFrom(address from, address to, uint256 value)
+        public returns (bool);
+
+    function approve(address spender, uint256 value) public returns (bool);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
 
 contract TakerProxy {
     uint256 constant MAX_UINT = 2**256 - 1;
@@ -18,7 +40,7 @@ contract TakerProxy {
 
     function setAllowance(address token_proxy, address[] token_addrs) public {
         for (uint i = 0; i < token_addrs.length; i++) {
-            StandardToken token = StandardToken(token_addrs[i]);
+            ERC20 token = ERC20(token_addrs[i]);
             require(token.approve(token_proxy, MAX_UINT), "Approve Error");
         }
     }
